51e311baca27b480a80486e63bb9fc5b37a7bad8
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2023-06-15 16:55:38 -0300

    grafico setores (cores)

--- src/app/home/grafico/grafico.component.ts
+++ src/app/home/grafico/grafico.component.ts
@@ -21,7 +21,8 @@ export class GraficoComponent implements OnInit {
     this.pieChartData.datasets[0].data[1] = 200
     this.pieChartData.datasets[0].data[2] = 300
     this.pieChartData.datasets[0].data[3] = 400
-    this.pieChartData.datasets[0].data[4] = 500
+    this.pieChartData.datasets[0].data[4] = 500 
+
   }
 
 
@@ -74,6 +75,7 @@ export class GraficoComponent implements OnInit {
         position: 'top',
       },
       datalabels: {
+        display: false,
         color: '#fff',
         formatter: (value, ctx) => {
           if (ctx.chart.data.labels) {
@@ -86,14 +88,14 @@ export class GraficoComponent implements OnInit {
   };
 
   public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: ['Clientes', 'Empréstimos', 'Devoluções', 'Riscos', 'Atributos'],
+    labels: [],
     datasets: [
       {
         backgroundColor: [
-          '#AC58FA', '#2E64FE', '#00CED1', '#FFBF00', '#819FF7'
+          '#AC58FA', '#2E64FE', '#00CED1', '#FFBF00', '#819FF7', '#708090', '#CD5C5C', '#00FA9A', '#00BFFF', '#32CD32'
         ],
         borderColor: [
-          '#AC58FA', '#2E64FE', '#00CED1', '#FFBF00', '#819FF7'
+          '#AC58FA', '#2E64FE', '#00CED1', '#FFBF00', '#819FF7', '#708090', '#CD5C5C', '#00FA9A', '#00BFFF', '#32CD32'
         ],
 
         data: [],
@@ -108,7 +110,6 @@ export class GraficoComponent implements OnInit {
 
 
   public async gerarPizza(valor: ProcessoRendaVariavel[], tickers: any) {
-    let kakaka: any[] = [];
     let repeticoes: any = [];
     let contador: any = {};
     let setorCout: any[] = [];
@@ -188,3 +189,108 @@ export class GraficoComponent implements OnInit {
   }
 
 }
+
+
+
+
+
+
+
+
+
+// import { Component, ViewChild } from '@angular/core';
+// import DatalabelsPlugin from 'chartjs-plugin-datalabels';
+// import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+// import { BaseChartDirective } from 'ng2-charts';
+
+// @Component({
+//   selector: 'app-pie-chart',
+//   templateUrl: './pie-chart.component.html',
+//   styleUrls: [ './pie-chart.component.scss' ]
+// })
+// export class PieChartComponent {
+//   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+//   // Pie
+//   public pieChartOptions: ChartConfiguration['options'] = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         display: true,
+//         position: 'top',
+//       },
+//       datalabels: {
+//         formatter: (value, ctx) => {
+//           if (ctx.chart.data.labels) {
+//             return ctx.chart.data.labels[ctx.dataIndex];
+//           }
+//         },
+//       },
+//     }
+//   };
+//   public pieChartData: ChartData<'pie', number[], string | string[]> = {
+//     labels: [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ],
+//     datasets: [ {
+//       data: [ 300, 500, 100 ]
+//     } ]
+//   };
+//   public pieChartType: ChartType = 'pie';
+//   public pieChartPlugins = [ DatalabelsPlugin ];
+
+//   // events
+//   public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+//     console.log(event, active);
+//   }
+
+//   public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+//     console.log(event, active);
+//   }
+
+//   changeLabels(): void {
+//     const words = [ 'hen', 'variable', 'embryo', 'instal', 'pleasant', 'physical', 'bomber', 'army', 'add', 'film',
+//       'conductor', 'comfortable', 'flourish', 'establish', 'circumstance', 'chimney', 'crack', 'hall', 'energy',
+//       'treat', 'window', 'shareholder', 'division', 'disk', 'temptation', 'chord', 'left', 'hospital', 'beef',
+//       'patrol', 'satisfied', 'academy', 'acceptance', 'ivory', 'aquarium', 'building', 'store', 'replace', 'language',
+//       'redeem', 'honest', 'intention', 'silk', 'opera', 'sleep', 'innocent', 'ignore', 'suite', 'applaud', 'funny' ];
+//     const randomWord = () => words[Math.trunc(Math.random() * words.length)];
+//     this.pieChartData.labels = new Array(3).map(_ => randomWord());
+
+//     this.chart?.update();
+//   }
+
+//   addSlice(): void {
+//     if (this.pieChartData.labels) {
+//       this.pieChartData.labels.push([ 'Line 1', 'Line 2', 'Line 3' ]);
+//     }
+
+//     this.pieChartData.datasets[0].data.push(400);
+
+//     this.chart?.update();
+//   }
+
+//   removeSlice(): void {
+//     if (this.pieChartData.labels) {
+//       this.pieChartData.labels.pop();
+//     }
+
+//     this.pieChartData.datasets[0].data.pop();
+
+//     this.chart?.update();
+//   }
+
+//   changeLegendPosition(): void {
+//     if (this.pieChartOptions?.plugins?.legend) {
+//       this.pieChartOptions.plugins.legend.position = this.pieChartOptions.plugins.legend.position === 'left' ? 'top' : 'left';
+//     }
+
+//     this.chart?.render();
+//   }
+
+//   toggleLegend(): void {
+//     if (this.pieChartOptions?.plugins?.legend) {
+//       this.pieChartOptions.plugins.legend.display = !this.pieChartOptions.plugins.legend.display;
+//     }
+
+//     this.chart?.render();
+//   }
+// }
