d9177039208b2ad8c5ae5714fa77cb3e4622e2ae
unknown <ezequielfernandes9122gmail.com>
2023-06-20 08:26:56 -0300

    atualizado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,11 +12,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
      this._service.gerarToken()
     // this._service.cadastroAAI()
-<<<<<<< HEAD
     this._service.cadastroCliente() 
-=======
-     this._service.cadastroCliente() 
->>>>>>> 4237cf6420dc8f83861a2815ee7d37284c201cb0
     // this._service.cadastroSetor()
     // this._service.cadastroTicker()
 
